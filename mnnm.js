08154cc845632a92521bec569a2fa18766f8bbbb
mnnm <alsrhld101@naver.com>
2021-12-23 14:37:48 +0900

    mnnm

---
+++ mnnm.js
@@ -0,0 +1,11 @@
+// const arr = ['가', '라', '다', '라', '마', '라'];
+// // console.log(arr.indexOf('라'));
+// // arr.splice(arr.indexOf('라'), 1);
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr.indexOf('라') !== -1) {
+//     arr.splice(arr.indexOf('라'), 1);
+//   }
+// }
+// console.log(arr);
+
+const multiply = (x, y, z) => x * y * z;
